335f345e52b05932c0bde625544ce771f4f44601
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2025-01-30 11:35:10 +0200

    test: to-do users tests

---
+++ e2e/users.spec.ts
@@ -0,0 +1,82 @@
+import { Page } from '@playwright/test'
+
+import { test } from './fixtures'
+
+test.describe.configure({ mode: 'parallel' })
+
+test.describe('users', () => {
+	test.describe('forceSetup is true', () => {
+		let page: Page
+		let teardown: VoidFunction
+		let baseURL: string
+
+		test.beforeAll(async ({ setup, browser, helpers }) => {
+			const setupResult = await setup({ forceSetup: true })
+			teardown = setupResult.teardown
+			baseURL = setupResult.baseURL
+			page = await browser.newPage()
+
+			await helpers.createFirstUser({ page, baseURL })
+			await helpers.setupTotp({ page, baseURL })
+			await page.goto(`${baseURL}/admin`)
+		})
+
+		test.afterAll(async () => {
+			await teardown()
+			await page.close()
+		})
+
+		test.describe('create new user', () => {
+			test.skip('should not see TOTP field', async () => {})
+
+			test.skip('should not be able to set totpSecret via API', async () => {})
+
+			test.skip('should not be able to set totpSecret via GraphQL', async () => {})
+		})
+
+		test.describe('get user', () => {
+			test.skip('should not see TOTP field', async () => {})
+
+			test.skip('should not be able to see totpSecret via API', async () => {})
+
+			test.skip('should not be able to see totpSecret via GraphQL', async () => {})
+		})
+	})
+
+	test.describe('forceSetup is false', () => {
+		let page: Page
+		let teardown: VoidFunction
+		let baseURL: string
+
+		test.beforeAll(async ({ setup, browser, helpers }) => {
+			const setupResult = await setup()
+			teardown = setupResult.teardown
+			baseURL = setupResult.baseURL
+			page = await browser.newPage()
+			await helpers.createFirstUser({ page, baseURL })
+			await helpers.setupTotp({ page, baseURL })
+			await page.goto(`${baseURL}/admin`)
+		})
+
+		test.afterAll(async () => {
+			await teardown()
+			await page.close()
+		})
+
+		test.describe('create new user', () => {
+			test.skip('should not see TOTP field', async () => {})
+
+			test.skip('should not be able to set totpSecret via API', async () => {})
+
+			test.skip('should not be able to set totpSecret via GraphQL', async () => {})
+		})
+
+		test.describe('get user', () => {
+			test.skip('should not see TOTP field', async () => {})
+
+			test.skip('should not be able to see totpSecret via API', async () => {})
+
+			test.skip('should not be able to see totpSecret via GraphQL', async () => {})
+		})
+	})
+})
